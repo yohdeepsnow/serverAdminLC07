5ae938a55d8517a3201ec03130924b56bbdebda3
yohdeepsnow <yohan.muliono@gmail.com>
2024-02-20 04:54:01 +0000

    membuat inisial program dengan fungsi penambahan

---
+++ main.py
@@ -0,0 +1,11 @@
+#tolong tambahkan pengurangan, pengalian dan pembagian dengan menggunakan function.
+
+def penambahan(a,b):
+	total = a+b
+	return total
+
+
+def main():
+	print(penambahan(10,5))
+
+main()
