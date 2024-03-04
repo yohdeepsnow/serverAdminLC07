a2754f439c9a1d5a5a9009e9d1ecae97a262a982
zhaxstronaaut <158799247+zhaxstronaaut@users.noreply.github.com>
2024-03-04 21:08:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ def perkalian(a,b):
 	total = a*b
 	return total
 
+def pembagian(a,b):
+	total = a/b
+	return total
+
 def main():
 	print(penambahan(10,5))
 	print(pengurangan(10,5))
 	print(perkalian(10,5))
+	print(pembagian(10,5))
 
 main()
